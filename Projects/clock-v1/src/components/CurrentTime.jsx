79d77c607b45c25e210a1c40dd1f5cef48f5e4a6
Maheera <maheerasaqib089@gmail.com>
2026-07-07 00:36:58 +0500

    finish Clock version 2

--- Projects/clock-v1/src/components/CurrentTime.jsx
+++ Projects/clock-v1/src/components/CurrentTime.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function CurrentTime() {
-  let time = new Date();
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    console.log("started");
+
+    const intervalID = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalID);
+      console.log("interval ended");
+    };
+  }, []);
+
   return (
     <p>
       The current time is: {time.toLocaleDateString()} -{" "}
