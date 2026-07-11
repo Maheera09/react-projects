652580282b6bcb65138c112bd97ba9a308eaaf58
Maheera <maheerasaqib089@gmail.com>
2026-07-11 14:34:45 +0500

    Finish Redux Basic

--- redux/1-only-redux/coding-redux.js
+++ redux/1-only-redux/coding-redux.js
@@ -7,4 +7,34 @@ const redux = require("redux");
 4. Actions
 */
 
-redux.createStore();
+const INITIAL_VALUE = {
+  counter: 0,
+};
+
+const reducer = (store = INITIAL_VALUE, action) => {
+  let newStore = store;
+  if (action.type === "INCREMENT") {
+    newStore = { counter: store.counter + 1 };
+  } else if (action.type === "DECREMENT") {
+    newStore = { counter: store.counter - 1 };
+  } else if (action.type === "ADDITION") {
+    newStore = { counter: store.counter + action.payload.number };
+  }
+  return newStore;
+}; //REDUCER
+
+const store = redux.createStore(reducer); //STORE
+
+const subscriber = () => {
+  //subsriber wo ha jo kehta ha mjy ye values chahiye and jab ye vlues change ho, UI repaint ho jaye
+
+  const state = store.getState(); //returns state.
+  console.log(state);
+}; //SUBSCRIBER
+
+store.subscribe(subscriber); //subscriber function idr store ko subscribe kr rha ha
+
+store.dispatch({ type: "INCREMENT" }); //ACTION
+store.dispatch({ type: "INCREMENT" }); //ACTION
+store.dispatch({ type: "DECREMENT" }); //ACTION
+store.dispatch({ type: "ADDITION", payload: { number: 7 } }); //ACTION
